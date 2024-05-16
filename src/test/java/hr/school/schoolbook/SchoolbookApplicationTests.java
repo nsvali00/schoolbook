d776f3ea3e90ee3dfac2ed5aa33902d2ad888bf8
nsvali00 <nikolasvalina001@gmail.com>
2024-05-16 14:59:10 +0200

    First commit

---
+++ src/test/java/hr/school/schoolbook/SchoolbookApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.school.schoolbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
